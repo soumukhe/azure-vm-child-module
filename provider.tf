f96c7a075cc3af739db0b269306406b6671f5bf3
soumukhe <soumukhe@cisco.com>
2021-04-02 18:37:15 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
+terraform {
+  required_version=">0.13"
+  required_providers {
+    azurerm = "~>2.0"
+  }
+}
+
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x.
-    # If you're using version 1.x, the "features" block is not allowed.
-    # version = "~>2.0"
-    features {}
+  features {}
 }
